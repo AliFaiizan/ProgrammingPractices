b79e3dd67db4aa4991ac468f0bdfd958dac70ed9
AliFaiizan <alifaizanahmad@gmail.com>
2022-09-06 03:08:13 +0500

    maxChar

---
+++ prep/maxChar/index.js
@@ -0,0 +1,31 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+
+function maxChar(str) {
+  let chars = {};
+
+  for (const char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+  let max = 0;
+  let maxChar = "";
+  for (const key in chars) {
+    if (chars[key] > max) {
+      max = chars[key];
+      maxChar = key;
+    }
+  }
+  return maxChar
+}
+
+
+module.exports = maxChar;
