d2a2ec295e2362242074976d63f0ec81ffe7412d
alifaiizan <alifaizanahmad@gmail.com>
2023-02-05 14:38:28 -0800

    array of products brute force

---
+++ algo.arrayOfProducts.js
@@ -0,0 +1,21 @@
+//[5,1,4,2]
+//[8,40,10,20]
+
+
+//brute force
+function arrayOfProducts(arr){
+    let result=[];
+    let sum=1;
+    for (i=0; i<arr.length ; i++) {
+        for(j=0 ; j< arr.length; j++){
+            if(j===i)
+            continue
+            sum = sum * arr[j]
+        }
+        result[i]=sum;
+        sum=1;
+    }
+    return result;
+}
+
+console.log(arrayOfProducts([5,1,4,2]))
