7c31e7c974690e83f54fecb8adfbff59ca37df9c
alifaiizan <alifaizanahmad@gmail.com>
2023-03-07 22:43:47 +0100

    algoarrayofproducts

--- algo.arrayOfProducts.js
+++ algo.arrayOfProducts.js
@@ -3,19 +3,35 @@
 
 
 //brute force
-function arrayOfProducts(arr){
-    let result=[];
-    let sum=1;
-    for (i=0; i<arr.length ; i++) {
-        for(j=0 ; j< arr.length; j++){
-            if(j===i)
-            continue
-            sum = sum * arr[j]
-        }
-        result[i]=sum;
-        sum=1;
+// function arrayOfProducts(arr){
+//     let result=[];
+//     let sum=1;
+//     for (i=0; i<arr.length ; i++) {
+//         for(j=0 ; j< arr.length; j++){
+//             if(j===i)
+//             continue
+//             sum = sum * arr[j]
+//         }
+//         result[i]=sum;
+//         sum=1;
+//     }
+//     return result;
+// }
+
+function arrayOfProducts(arr) {
+  let result = [];
+  let leftProduct=[];
+  let rightProduct=[]
+  let sum = 1;
+  for (i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      if (j === i) continue;
+      sum = sum * arr[j];
     }
-    return result;
+    result[i] = sum;
+    sum = 1;
+  }
+  return result;
 }
 
 console.log(arrayOfProducts([5,1,4,2]))
