c275eb4f8fe1cd1566137233e7dd6ce7c065f484
alifaiizan <alifaizanahmad@gmail.com>
2023-03-12 22:15:52 +0100

    py.algoarrayOfProduct.py

---
+++ algo.arrayOfProduct.py
@@ -0,0 +1,13 @@
+# array of product efficient solution
+
+def arrayOfProduct(array):
+    products = [1 for _ in range(len(array))]
+    leftRunningProduct = 1
+    for i in range(len(array)):
+        products[i] = leftRunningProduct
+        leftRunningProduct *= array[i]
+    rightRunningProduct = 1
+    for i in reversed(range(len(array))):
+        products[i] *= rightRunningProduct
+        rightRunningProduct *= array[i]
+    return products
